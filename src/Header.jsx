d75ce57e069b801aaabf23c8dabb577a9e791e70
SAM ADKINS <sjamesadkins@gmail.com>
2023-06-30 22:33:53 -0500

    Initializing personal site in REACT
     On branch main

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+
+    return (
+        <header>
+            <div>
+                <h1 style={{fontSize: "40pt"}}>
+                    Sam Adkins
+                </h1>
+                <p style={{fontSize: "20pt"}}>
+                    <a href='http://www.linkedin.com/in/adkins-mba'>LinkedIn</a><br></br>
+                    <a href='http://www.linkedin.com/in/adkins-mba'>GitHub</a><br></br>
+                </p>
+            </div>
+        </header>
+    ) 
+}
+
+export default Header
