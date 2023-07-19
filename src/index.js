41bef7e608921f7344a84ed4255a5967b569f411
SAM ADKINS <sjamesadkins@gmail.com>
2023-07-18 21:41:16 -0500

    troubleshooting github pages deployment

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ import ErrorPage from './components/ErrorPage';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <HashRouter basename="/">
+    <HashRouter basename="/adkins-site">
       <Routes>
           <Route path='/' element={<Landing />}/>
           <Route path='/about' element={<About />}/>
